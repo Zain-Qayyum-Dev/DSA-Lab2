45efa436e3ee30eb331e5f81ea11c431d838558b
Zain Qayyum <zainqayyum145175@email.com>
2026-09-18 16:48:54 +0500

    Correcting the logic of the duplicate cases

--- TestDrivenProblem.cpp
+++ TestDrivenProblem.cpp
@@ -4,7 +4,7 @@ using namespace std ;
 bool isSorted(const int* arr, const int size){
 
     for ( int i = 0 ; i < size-1 ; i++ ){
-        if( *( arr + i ) >= *( arr + i + 1) ){
+        if( *( arr + i ) > *( arr + i + 1) ){
             return false ;
         }
     }
